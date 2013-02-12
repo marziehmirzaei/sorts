fa4b765f0d185f9b1910af538187865fed47b1ea
Derek Wene <djrobotfreak@tamu.edu>
2013-02-11 23:04:16 -0600

    Insertion Sort
    Untested insertion sort I wrote today in lab

--- 221-12c-A2-code/insertion-sort.cpp
+++ 221-12c-A2-code/insertion-sort.cpp
@@ -14,4 +14,20 @@ InsertionSort::sort(int A[], int size)				// main entry point
   /* Complete this function with the implementation of insertion sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
   */
-}
+  int temp;
+	for(int i=1; i<size-1; ++i)
+	{
+		if(A[i]<A[i-1])
+		{
+			int temp=2;
+			while(A[i]<A[i-temp]&&(i-temp)>=0])
+			{
+			++temp;
+			}
+		temp=A[i-temp];
+		A[i]=temp;
+		temp=A[i];
+		}
+	
+	}
+  }
