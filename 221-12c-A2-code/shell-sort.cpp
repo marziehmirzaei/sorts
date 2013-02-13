bed5135a571fe34ea0ae4cb988f17dd5df0a8c4e
rant <rwatsonxc@gmail.com>
2013-02-12 22:03:17 -0600

    shell sort implemented, but not tested

--- 221-12c-A2-code/shell-sort.cpp
+++ 221-12c-A2-code/shell-sort.cpp
@@ -12,7 +12,25 @@
 void
 ShellSort::sort(int A[], int size)
 {
-  /* Complete this function with the implementation of shell sort algorithm 
-  Record number of comparisons in variable num_cmps of class Sort
-  */
+ num_cmps = 0;
+ int step = size/2 -1;
+ while(step >= 1)
+ {
+ for(int j = 0; j < step -1; ++j)
+	 for(int n = step + j; n < size; n += step)
+	 {
+		if(A[n] < A[n - step])
+		{
+			int index = n;
+			while(A[index] < A[index - step])
+			{
+				int temp = A[index];
+				A[index] = A[index - step];
+				A[index - step] = temp;
+				index -= step;
+			}
+		}
+	 }
+	 step = (step + 1) / 2 + 1;
+	 }
 }
